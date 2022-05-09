7193c77b05fa89dbf488ea32b55f4ae6a6b7ba85
Divya945 <divyadvs945@gmail.com>
2022-05-09 12:43:29 +0530

    1 file modified, 1 added

--- src/Components/StatewithHooksComponent/Items.js
+++ src/Components/StatewithHooksComponent/Items.js
@@ -1,5 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Items.css';
 export default function Items() {
-  return <div>fdfd</div>;
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  return (
+    <div className="wrapper">
+      <div>Shopping Cart: {cart.length} total items.</div>
+      <div>Total: {total}</div>
+      <div className="product">
+        <span role="img" aria-label="ice cream">
+          🍦
+        </span>
+      </div>
+      <button>Add</button> <button>Remove</button>
+    </div>
+  );
 }
